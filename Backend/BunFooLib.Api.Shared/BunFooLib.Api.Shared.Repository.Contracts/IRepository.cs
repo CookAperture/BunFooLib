28e4ce8da41d19ad05cd0f521432140a6f8048e3
SonicYeager <69338558+SonicYeager@users.noreply.github.com>
2022-06-18 17:40:35 +0200

    Add Repository with extra abstraction layer;
    * AssemblyInfo incomplete;

---
+++ Backend/BunFooLib.Api.Shared/BunFooLib.Api.Shared.Repository.Contracts/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunFooLib.Api.Shared.Repository.Contracts
+{
+    public interface IRepository<T> where T : class
+    {
+        void Create(T entity);
+        T? ReadById(int id);
+        IEnumerable<T> Read();
+        IEnumerable<T> ReadByExpression(Expression<Func<T, bool>> predicate);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Backend/BunFooLib.Api.Shared/BunFooLib.Api.Shared.Repository/Repository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunFooLib.Api.Shared.Repository
+{
+    internal class Repository<TEntity, TContext> : IRepository<TEntity> 
+        where TEntity : class 
+        where TContext : DbContext
+    {
+        private readonly TContext _dbContext;
+
+        public Repository(TContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public virtual TEntity? GetById(int id)
+        {
+            return _dbContext.Set<TEntity>().Find(id);
+        }
+
+        public virtual IEnumerable<TEntity> List()
+        {
+            return _dbContext.Set<TEntity>().AsEnumerable();
+        }
+
+        public virtual IEnumerable<TEntity> List(System.Linq.Expressions.Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbContext.Set<TEntity>()
+                   .Where(predicate)
+                   .AsEnumerable();
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
+
+        public virtual void Delete(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public virtual void Add(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Add(entity);
+            _dbContext.SaveChanges();
+        }
+    }
+}
